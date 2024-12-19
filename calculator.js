2d749d6289f8042f497fb526fef370a10026dc00
Aria Shi <ariashi15@gmail.com>
2024-12-19 02:03:09 -0500

    added some (buggy) calculating functionality

--- calculator.js
+++ calculator.js
@@ -1,5 +1,19 @@
+// parse: parses if the input string is a valid combination of numbers and operators
+function parse(inputString) {
+    const regex = /(\d+(\.\d+)?[\+−×÷])(\d+(\.\d+)?)/;
+    if (regex.test(inputString)) { // valid expression
+        const matcher = /(\d+(\.\d+)?)|([\+−×÷])/g;
+        return inputString.match(matcher); // return array in the form ["1", "+", "1"]
+    } else {
+        return []; // not a valid input, so return empty array
+    }
+}
+
 // operate: operates on two numbers
 function operate(a, b, operator) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+
     if (operator === "+") {
         return a + b;
     } else if (operator === "−") {
@@ -11,6 +25,20 @@ function operate(a, b, operator) {
     }
 }
 
+// calculate: goes through the parsed expression and calculates with the operations from left to right
+function calculate(arr) {
+    let accumulator = arr[0]; // start with the first number
+    opIndex = 1;
+    numIndex = 2;
+    while (numIndex < arr.length) {
+        accumulator = operate(accumulator, arr[numIndex], arr[opIndex])
+        numIndex += 2;
+        opIndex += 2;
+    }
+
+    return accumulator;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const allButtons = document.querySelectorAll(".button");
     let displayText = "";
@@ -20,8 +48,15 @@ document.addEventListener("DOMContentLoaded", () => {
             let val = button.textContent.trim();
 
             if (val === "=") {
-                // display the new value
-            } else if (val === "Clear" | val === "On/Off") {
+                let parsedExpr = parse(displayText);
+
+                if (parsedExpr == []) {
+                    displayText = "invalid expression";
+                } else {
+                    displayText = calculate(parsedExpr);
+                }
+                
+            } else if (val === "Clear" || val === "On/Off") {
                 displayText = "";
             } else {
                 displayText += val;
